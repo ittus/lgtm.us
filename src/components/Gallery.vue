e6f0b0fdcab8558489bcfdb404182fcff89e7382
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-05 14:45:14 +0900

    Add refresh button

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,8 +1,13 @@
 <template>
-  <div
-    class="d-flex flex-wrap flex-items-stretch flex-justify-center container-xl gutter-sm-condensed gutter-md mx-auto p-responsive"
-  >
-    <gallery-item v-for="idx in 8" :key="idx" />
+  <div>
+    <div class="d-flex flex-row-reverse mx-4">
+      <button class="btn mr-2" type="button" @click="refresh">Refresh</button>
+    </div>
+    <div
+      class="d-flex flex-wrap flex-items-stretch flex-justify-center container-xl gutter-sm-condensed gutter-md mx-auto p-responsive"
+    >
+      <gallery-item v-for="idx in 8" :key="idx" ref="galleryItem" />
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,14 @@ import GalleryItem from "./GalleryItem.vue";
 export default {
   components: {
     GalleryItem
+  },
+  methods: {
+    refresh() {
+      if (!this.$refs.galleryItem) {
+        return;
+      }
+      this.$refs.galleryItem.forEach(item => item.getImage());
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <gallery />
+    <gallery class="mt-5" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Gallery from "@/components/Gallery.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
     Gallery
   }
 };
