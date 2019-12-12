b3591b4cf3e6e6f50d494efea7e79a54ec078a00
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-12 17:46:04 +0900

    Update how to use

--- src/views/HowToUse.vue
+++ src/views/HowToUse.vue
@@ -33,5 +33,137 @@
         <img src="../assets/images/sample_review.jpeg" alt="review result" />
       </li>
     </ol>
+    <div>
+      <div class="container-xl clearfix">
+        <div class="col-sm-12 col-md-6 float-left pr-5">
+          <h1>
+            Open source
+          </h1>
+          <div class="mb-4 lead-mktg">
+            lgtm.us is open-sourced on GitHub. Contributions and feedback are
+            welcome!
+          </div>
+          <a
+            href="https://github.com/ittus/lgtm.us"
+            class="btn-github"
+            target="_blank"
+          >
+            <svg
+              aria-hidden="true"
+              height="20"
+              role="img"
+              viewBox="0 0 16 16"
+              width="20"
+              style="display:inline-block;fill:currentColor;user-select:none;vertical-align:text-bottom"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+              ></path>
+            </svg>
+            Contribute on GitHub
+          </a>
+        </div>
+        <div class="col-sm-12 col-md-6 float-left">
+          <h1
+            color="black"
+            font-size="7"
+            font-weight="bold"
+            class="Heading-sc-1m35ws5-0 hPIWbN"
+          >
+            Keep in touch
+          </h1>
+          <a
+            font-size="2"
+            display="block"
+            href="https://twitter.com/ittus_fpt"
+            class="social-link d-block"
+            target="_blank"
+          >
+            <svg
+              aria-hidden="true"
+              class="social-icon"
+              height="20"
+              role="img"
+              viewBox="0 0 20 20"
+              width="20"
+              style="display:inline-block;fill:currentColor;user-select:none;vertical-align:text-top"
+            >
+              <path
+                d="M6.28976 18C13.8373 18 17.9648 11.845 17.9648 6.50756C17.9648 6.33274 17.9611 6.1587 17.9532 5.98546C18.7544 5.41531 19.4507 4.70382 20 3.89388C19.2648 4.21557 18.4736 4.43213 17.6436 4.52978C18.4908 4.02972 19.1412 3.23869 19.448 2.2957C18.6552 2.75835 17.7772 3.09461 16.8424 3.27615C16.0936 2.49105 15.0276 2 13.8472 2C11.5812 2 9.74363 3.80889 9.74363 6.03861C9.74363 6.35557 9.77963 6.66387 9.85003 6.9596C6.43968 6.79068 3.41563 5.18341 1.39176 2.73945C1.03936 3.33636 0.836164 4.02974 0.836164 4.76959C0.836164 6.17093 1.56058 7.40804 2.66218 8.13177C1.98897 8.1113 1.35656 7.92938 0.803816 7.62659C0.803212 7.64354 0.80321 7.66006 0.80321 7.67817C0.80321 9.63432 2.21762 11.2675 4.09518 11.6376C3.75038 11.7302 3.38761 11.7798 3.01318 11.7798C2.74918 11.7798 2.49198 11.7542 2.24198 11.7069C2.76438 13.3118 4.2792 14.4797 6.07515 14.5123C4.67074 15.5959 2.90157 16.2413 0.97878 16.2413C0.647979 16.2413 0.321182 16.2228 -1.71715e-06 16.1854C1.81601 17.3312 3.97236 17.9997 6.28998 17.9997"
+              ></path>
+            </svg>
+            Follow me on Twitter
+          </a>
+          <a
+            font-size="2"
+            display="block"
+            href="https://www.buymeacoffee.com/8buMYCOog"
+            class="social-link d-block"
+            target="_blank"
+          >
+            <svg
+              height="20"
+              class="octicon octicon-heart social-icon"
+              viewBox="0 0 12 16"
+              version="1.1"
+              width="15"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2c-.97 0-1.69.42-2.2 1-.51.58-.78.92-.8 1-.02-.08-.28-.42-.8-1-.52-.58-1.17-1-2.2-1-1.632.086-2.954 1.333-3 3 0 .52.09 1.52.67 2.67C1.25 8.82 3.01 10.61 6 13c2.98-2.39 4.77-4.17 5.34-5.33C11.91 6.51 12 5.5 12 5c-.047-1.69-1.342-2.913-3-3z"
+              ></path>
+            </svg>
+            Support me
+          </a>
+          <a
+            font-size="2"
+            display="block"
+            href="https://github.com/ittus/lgtm.us/issues/new"
+            target="_blank"
+            class="social-link d-block"
+          >
+            <svg
+              aria-hidden="true"
+              class="social-icon"
+              height="20"
+              role="img"
+              viewBox="0 0 16 16"
+              width="20"
+              style="display:inline-block;fill:currentColor;user-select:none;vertical-align:text-top"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.7 5.34c.13-.32.55-1.59-.13-3.31 0 0-1.05-.33-3.44 1.3-1-.28-2.07-.32-3.13-.32s-2.13.04-3.13.32c-2.39-1.64-3.44-1.3-3.44-1.3-.68 1.72-.26 2.99-.13 3.31C.49 6.21 0 7.33 0 8.69 0 13.84 3.33 15 7.98 15S16 13.84 16 8.69c0-1.36-.49-2.48-1.3-3.35zM8 14.02c-3.3 0-5.98-.15-5.98-3.35 0-.76.38-1.48 1.02-2.07 1.07-.98 2.9-.46 4.96-.46 2.07 0 3.88-.52 4.96.46.65.59 1.02 1.3 1.02 2.07 0 3.19-2.68 3.35-5.98 3.35zM5.49 9.01c-.66 0-1.2.8-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.54-1.78-1.2-1.78zm5.02 0c-.66 0-1.2.79-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.53-1.78-1.2-1.78z"
+              ></path>
+            </svg>
+            Share feedback on GitHub
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.btn-github {
+  background-color: rgb(27, 31, 35);
+  font-size: 16px;
+  font-weight: 600;
+  display: inline-block;
+  margin-right: 8px;
+  color: rgb(200, 225, 255) !important;
+  text-decoration: none;
+  padding: 0.75em 1.25em;
+  border-radius: 6px;
+}
+
+.social-link {
+  color: rgb(27, 31, 35) !important;
+  margin-bottom: 1rem;
+  .social-icon {
+    margin-right: 0.5rem;
+  }
+}
+</style>
