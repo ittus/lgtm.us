60161236e61bc03cc3984c808689f9e9f128b47d
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-12-18 18:18:22 +0900

    Add gtm

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import router from "./router";
 import store from "./store";
 import "./assets/styles/index.scss";
 import { auth } from "./firebase";
+import VueGtm from "vue-gtm";
 
 Vue.config.productionTip = false;
+
+if (process.env.VUE_APP_GTM_ID) {
+  Vue.use(VueGtm, {
+    id: process.env.VUE_APP_GTM_ID,
+    enabled: true,
+    debug: process.env.NODE_ENV !== "production",
+    loadScript: true,
+    vueRouter: router,
+    trackOnNextTick: true
+  });
+}
 
 let app;
 auth.onAuthStateChanged(user => {
