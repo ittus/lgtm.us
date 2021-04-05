30a624f269b3e63d3db075d1b6afdd83d89ea587
Vu Minh Thang <vuminhthang.cm@gmail.com>
2021-04-05 19:17:40 +0900

    deps: update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
 
 // firebase init goes here
 const config = {
